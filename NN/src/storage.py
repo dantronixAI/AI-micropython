e3dcceb4ca8dafb9abb998f0c7dd34d18284afcd
gadan <danielastudilloheras@gmail.com>
2022-04-18 12:31:59 +0200

    Update storage.py

--- NN/src/storage.py
+++ NN/src/storage.py
@@ -36,6 +36,39 @@ def load_layer(n_layer,ltype):
     f.close()
     return array
 
+def save_2darray(array,name):
+    f = open(path+"output/{}".format(name), "w")
+    shape = array.shape
+    f.write("{}: {}".format(name,shape))
+    f.write("\n")
+    for row in range(shape[0]):
+        for col in range(shape[1]):
+            f.write("{}".format(array[row][col],))
+            if not ((row==shape[0]-1) and (col==shape[1]-1)):
+                f.write(",")
+    f.close()
+
+def load_2darray(name):
+    f = open(path + "output/{}".format(name), "r")
+    line = f.readline()
+    ini_pos = line.find("(")
+    end_pos = line.find(")")
+    structure = line[ini_pos + 1:end_pos]
+    dimentions = structure.split(",")
+    rows = int(dimentions[0])
+    cols = int(dimentions[1])
+    array = np.zeros((rows, cols))
+    line = f.readline()
+    line = line.replace("\n", "")
+    line = line.split(",")
+    c_line = 0
+    for row in range(rows):
+        for col in range(cols):
+            array[row][col] = float(line[c_line])
+            c_line += 1
+    f.close()
+    return array
+
 
 def save_weights(nn,name):
     data = nn.weights
